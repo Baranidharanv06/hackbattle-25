d140c4326522d6b8f82091983cf1ea2c7ace16dd
Baranidharan <baranidharanviswanathan2006@gmail.com>
2025-09-19 16:19:17 +0530

    webm to gif in FAQ

--- src/components/CharacterDisplay.js
+++ src/components/CharacterDisplay.js
@@ -1,22 +1,25 @@
 'use client';
+
 import React from 'react';
+import Image from 'next/image';
 
 export default function CharacterDisplay({ characterSrc, className, style }) {
   return (
-    <div className="relative w-full max-w-xs mx-auto aspect-square">
-      <video
+    <div
+      className={`relative w-full max-w-xs mx-auto aspect-square ${className}`}
+      style={style}
+    >
+      <Image
         key={characterSrc}
         src={characterSrc}
-        autoPlay
-        loop
-        muted
-        playsInline
-        className={`absolute top-0 left-0 w-full h-full object-contain select-none animate-fade-in-out ${className}`}
-        style={style}
+        alt="Character Animation"
+        fill 
+        style={{ objectFit: 'contain' }}
+        className="select-none animate-fade-in-out"
+        unoptimized={true} 
         draggable="false"
         onDragStart={(e) => e.preventDefault()}
       />
     </div>
   );
 }
-
